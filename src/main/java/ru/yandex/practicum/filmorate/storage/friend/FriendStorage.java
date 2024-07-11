e992d9b19646aa071d2571f81a46d04a6a471236
Nikita <roboromapie@gmail.com>
2024-07-11 23:41:09 +0300

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/friend/FriendStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/friend/FriendStorage.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface FriendStorage {
     boolean addInFriends(User friendRequest, User friendResponse);
+
     boolean deleteFromFriends(User friendRequest, User friendResponse);
+
     List<Long> findFriends(long id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -2,15 +2,18 @@ package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
     Optional<User> create(User user);
+
     Optional<User> update(User user);
+
     boolean delete(User user);
+
     List<User> findUsers();
+
     Optional<User> findUserById(long userId);
 
 }
